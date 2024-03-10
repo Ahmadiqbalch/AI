744b62a93848ad85a95b3d1d295a9c452d503ed5
Ahmad Iqbal Ch <ch5523273@gmail.com>
2024-03-10 21:45:52 +0500

    Assignment 3

---
+++ Lab 2/datatype.py
@@ -0,0 +1,83 @@
+# integer_var = 10
+# float_var = 3.14
+# string_var = 'hello'
+# boolean_var = True
+
+# # Printing the variables
+# print("Integer Variable:", integer_var)
+# print("Float Variable:", float_var)
+# print("String Variable:", string_var)
+# print("Boolean Variable:", boolean_var)
+
+# -------------------------------------------------------------
+
+# integer_var = 10
+
+# float_var = float(integer_var)
+
+# print("Float Variable after Type Casting:", float_var)
+
+# # Type casting from float to int
+
+# float_var = 3.14
+
+# integer_var = int(float_var)
+
+# print("Integer Variable after Type Casting:", integer_var)
+
+# # Type casting from int to string
+
+# integer_var = 10
+
+# string_var = str(integer_var)
+
+# print("String Variable after Type Casting:", string_var)
+
+# ---------------------------------------------------------------------
+
+# X = 10
+
+# y = 3
+
+# print("Addition:", x + y)
+
+# print("Subtraction:", x - y)
+
+# print("Multiplication:", x*y)
+
+# print("Division:", x/y)
+
+# print("Modulus:", x% y)
+
+# print("Exponentiation:", x** y)
+
+# # Logical operators example
+
+# a = True
+
+# b = False
+
+# print("AND:", a and b)
+
+# print("OR:", a or b)
+
+# print("NOT:", not a)
+
+
+# ---------------------------------------------------
+
+# x = 10
+# if x > 0:
+#     print("x is positive")
+# else:
+#      print("x is non positive")
+# y = -5
+# if y > 0:
+#     print("y is positive")
+# elif y == 0:
+#     print("y is zero")
+# else:
+#     print("y is negative")
+    
+# -----------------------------------------------
+
